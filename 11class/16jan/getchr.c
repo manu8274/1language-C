359a72c72ffb3a889ddf6fb891f98746ef5cafeb
manu8274 <manu1238274@gmail.com>
2023-01-16 10:27:47 +0530

    commit

---
+++ 11class/16jan/getchr.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+    int d;
+    printf("\n enter;");
+    d = getwchar();
+    printf("value=:");
+    putwchar(d);
+}
--- letusc/whilepg87probB_b.c
+++ letusc/whilepg87probB_b.c
@@ -2,21 +2,26 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include<math.h>
 
 void main(){
-    long int i=1,len,sum=0,orignum;
-    char num[0];
+    long int i=1,orignum;
+    float len,sum=0;
+    char num[100];
     printf("\n enter the number: ");
     scanf("%s",num);
     len=strlen(num);
     int num1=atoi(num);
+    int x;
+    x=(int)len;
     orignum=num1;
-    while (i<=len)
+    while (i<=x)
     {
-        int d;
+        float d;
         d=num1%10;
         num1=num1/10;
-        sum=sum + (d*d*d);
+        d=pow(d,len);
+        sum=sum + d;
         i+=1;
     }
     if (orignum==sum)
