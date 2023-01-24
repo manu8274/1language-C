5420aa47a9efee3e5046d01ab8bc2274b31dd63b
manu8274 <manu1238274@gmail.com>
2023-01-24 10:00:32 +0530

    commit

---
+++ 11class/23jan/4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    int a=2;
+    switch (a)
+    {
+        case 1:
+            printf("val is 1");
+            break;
+        case 2:
+            printf("val is 2");
+            break;
+        case 3:
+            printf("val is 3");
+            break;
+        default:
+            printf("val is 7");
+            break;
+    }
+    return 0;
+}
