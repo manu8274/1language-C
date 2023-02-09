f0d86d38a7d78eef67e4ae2277e651ff2778fa4f
manu8274 <manu1238274@gmail.com>
2023-02-09 09:36:48 +0530

    commit

--- letusc/for/EX_B_c.c
+++ letusc/for/EX_B_c.c
@@ -1,7 +1,17 @@
 #include<stdio.h>
+#include<math.h>
+
 int main()
 {
-    int a,p,r,n,q;
-    
+    int principle,time;
+    float amount,rate;
+    printf("enter the value of principle, time period and rate:");
+    scanf("%d %d %f",&principle,&time,&rate);
 
+    amount=0.0;
+    for (int i=1;i<=time;i++)
+    {
+        amount=principle*(pow((1+rate/100),time));
+    }
+    printf("final amount is %f.",amount);
 }
