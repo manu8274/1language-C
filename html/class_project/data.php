92f84bffcb3910c9b6d7c5cf2c22878b83dac267
manu8274 <manu1238274@gmail.com>
2023-04-18 17:05:48 +0530

    commit

---
+++ html/class_project/data.php
@@ -0,0 +1,12 @@
+<html>
+    <body>
+        Name = <?php echo $_POST["fname"]; ?>
+        Roll no. = <?php echo $_POST["froll"]; ?>
+        Email = <?php echo $_POST["femail"]; ?>
+        Semester = <?php echo $_POST["fsemester"]; ?>
+        Your one word = <?php echo $_POST["fyou"]; ?>
+        Rating = <?php echo $_POST["frate"]; ?>
+        Club = <?php echo $_POST["fclub"]; ?>
+        feedback = <?php echo $_POST["feedback"]; ?>
+    </body>
+</html>
