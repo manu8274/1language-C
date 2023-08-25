e468797d68921947136037139af2f289217e9245
manu8274 <manu8274@gmail.com>
2023-08-25 18:06:04 +0530

    commit

--- 2nd_YR/3rd_SEM/oops/08_25_23/1.c
+++ 2nd_YR/3rd_SEM/oops/08_25_23/1.c
@@ -52,16 +52,37 @@
 //     details(s1);
 // }
 
+// struct stu{
+//     int roll;
+//     char name[10];
+// };
+// void display(struct stu s2){
+//     printf("your name:%s\t",s2.name);
+//     printf("your name:%d",s2.roll);
+// }
+// int details(struct stu *s1){
+//     printf("enter name:");
+//     scanf("%s",s1->name);//array of character so index changes so not used ampersand
+//     printf("\nenter roll:");
+//     scanf("%d",&s1->roll);
+
+// }
+// int main(){
+//     struct  stu s1;
+//     details(&s1);
+//     display(s1);
+// }
+
 struct stu{
     int roll;
     char name[10];
 };
 void display(struct stu s2){
     printf("your name:%s\t",s2.name);
-    printf("your name:%d",s2.roll);
+    printf("your roll:%d",s2.roll);
 }
 int details(struct stu *s1){
-    printf("enter name:");
+    printf("\nenter name:");
     scanf("%s",s1->name);//array of character so index changes so not used ampersand
     printf("\nenter roll:");
     scanf("%d",&s1->roll);
@@ -69,6 +90,11 @@ int details(struct stu *s1){
 }
 int main(){
     struct  stu s1;
-    details(&s1);
-    display(s1);
+    int nos;
+    printf("enter the number of students:");
+    scanf("%d",&nos);
+    for(int i=0;i<nos;i++){
+        details(&s1);
+        display(s1);
+    }
 }
