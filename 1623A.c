7c7f537d6a1b5cf6150d6d7940dbf2bb53c820ee
manu8274 <manu8274@gmail.com>
2023-08-27 18:11:30 +0530

    commit

---
+++ 1623A.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int m,n,rx,ry,dx,dy;
+        scanf("%d %d %d %d %d %d",& n,&m,&rx,&ry,&dx,&dy);
+        
+    }
+}
---
+++ codeforces/08_27_23/1623A.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int main(){
+    int t;
+    scanf("%d",&t);
+    for(int i=0;i<t;i++){
+        int m,n,rr,rc,dr,dc;
+        scanf("%d %d %d %d %d %d",&n,&m,&rr,&rc,&dr,&dc);
+        if(rr==dr || rc==dc){
+            printf("%d\n",0);
+        }
+        else if(rc<dc && rr>dr){
+            printf("%d\n",dc-rc);
+        }
+        else if(rr<dr && rc>dc){
+            printf("%d\n",dr-rr);
+        }
+        else if(rc<dc && rr<dr){
+            if((dc-rc)<(dr-rr)){
+                printf("%d\n",dc-rc);
+            }
+            else{
+                printf("%d\n",dr-rr);
+            }
+        }
+        else if(rr>dr && rc>dc){
+            int z;
+            if((n-rr) > (m-rc)){
+                z=n-rr;
+            }
+            else if((n-rr) < (m-rc)){
+                z=m-rc;
+            }
+            else{
+                z=(n-rr) + (m-rc);
+            }
+            int a=rr-dr,b=rc-dc;
+            if(a>b){
+                printf("%d\n",z+a);
+            }
+            else{
+                printf("%d\n",z+b);
+            }
+        }
+    }
+}
