7035033f9d13c6eeaffafe0ebe7e4b96693c476b
manu8274 <manu8274@gmail.com>
2023-09-01 16:34:07 +0530

    commit

---
+++ 2nd_YR/3rd_SEM/oops/08aug/08_31_23/1_important.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int sum(int a,int b){
+    printf("operator sum is = %d",a+b);
+}
+int div(int a,int b){
+    printf("operator division is = %d",a/b);
+}
+int product(int a,int b){
+    printf("operator product is = %d",a*b);
+}
+int sub(int a,int b){
+    printf("operator sub is = %d",a-b);
+}
+
+int main(){
+    int a,b,c;
+    printf("Enter number");
+    scanf("%d %d",&a,&b);
+    int (*ptr[])(int,int)={sum,div,product,sub};//ie 0 index=sum funct , 1 index=div func,2index=
+    printf("\nenter your choice (0,1,2,3,4):");
+    scanf("%d",&c);
+    while(c<4){
+        // (*ptr)(a,b);for only 1st index
+        ptr[c](a,b);
+        printf("\nenter your choice (1,2,3,4):");
+        scanf("%d",&c);
+    }
+}
