9f96234d1972dd83e2b446cc1e19b7e069f3bfff
manu8274 <manu8274@gmail.com>
2023-09-11 12:53:27 +0530

    commit

--- 2nd_YR/3rd_SEM/dbms/student.sql
+++
@@ -1,9 +0,0 @@
-create table student2(
-	sid int(10) primary key,
-	first_name varchar(50),
-	last_name varchar(50),
-	birthdate date,
-	address varchar(50)
-	);
-insert into student values (1,"m","n",'22-10-1987',"india"),(2,"a","b",'21-10-1987',"india");
-select * from student;
