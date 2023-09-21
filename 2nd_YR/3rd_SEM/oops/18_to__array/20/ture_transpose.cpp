738034a9a96c7963a5fc1cd2c53d61d2b6314aa8
manu8274 <manu8274@gmail.com>
2023-09-21 17:36:22 +0530

    commit

--- 2nd_YR/3rd_SEM/oops/18_to__array/20/ture_transpose.cpp
+++ 2nd_YR/3rd_SEM/oops/18_to__array/20/ture_transpose.cpp
@@ -16,15 +16,22 @@ int main(){
     cout<<"before change:"<<endl;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            cout<<arr[i][j];
+            cout<<arr[i][j]<<"\t";
         }
         cout<<endl;
     }
-    
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n/2;j++){
+            int temp = arr[i][j];
+            arr[i][j]=arr[i][n-j-1];
+            arr[i][n-1-j]=temp;
+        }
+    }
     cout<<"changed array:"<<endl;
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
-            cout<<arr[i][j];
+            cout<<arr[i][j]<<"\t";
         }
         cout<<endl;
     }
