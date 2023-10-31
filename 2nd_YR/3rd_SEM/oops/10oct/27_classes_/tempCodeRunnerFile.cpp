624c37173e570b36d28e7278c6b4a4ea04f4a626
manu8274 <manu8274@gmail.com>
2023-10-31 12:55:20 +0530

    commit

--- 2nd_YR/3rd_SEM/oops/10oct/27_classes_/tempCodeRunnerFile.cpp
+++ 2nd_YR/3rd_SEM/oops/10oct/27_classes_/tempCodeRunnerFile.cpp
@@ -23,10 +23,85 @@ using namespace std;
 //     cout<<st1.x;
 // }
 
+// int main(){
+//     // int *ptr=NULL;//null pointer
+//     // int a=10;
+//     // cout<<&a<<endl;
+//     // cout<<"val of ptr "<<ptr<<endl;
+//     // cout<<"val of *ptr"<<*ptr<<endl;
+
+
+//     void *ptr=NULL;//void pointer , cannot print directly so to type cast as cout<<*ptr; does not work
+//     int a=10;
+//     float b=10.2;
+//     ptr=&a;
+//     cout<<"address of a "<<&a<<endl;
+//     cout<<"val of ptr "<<ptr<<endl;
+//     cout<<"val of *ptr "<<*((int*)ptr)<<endl;
+
+//     ptr=&b;
+//     cout<<"address of b "<<&b<<endl;
+//     cout<<"val of ptr "<<ptr<<endl;
+//     cout<<"val of *ptr "<<*((float*)ptr)<<endl;
+// }
+
+
+// int main(){
+//     string str;
+//     cout<<"enter the string : ";
+//     cin>>str;
+//     cout<<"enter our coice of left or right rotation : ";
+//     string choice;
+//     cin>>choice;
+//     int s=str.size();
+//     if(choice=="left"){
+//         for(int i=1;i<s;i++){
+//             cout<<str[i];
+//         }
+//         cout<<str[0];
+//     }
+//     else{
+//         string temp;
+//         cout<<str[s-1];
+//         for(int i=0;i<s-1;i++){
+//             cout<<str[i];
+//         }
+//     }
+// }
+
+
+
 int main(){
-    int *ptr=NULL;//null pointer
-    int a=10;
-    cout<<&a<<endl;
-    cout<<"val of ptr "<<ptr<<endl;
-    cout<<"val of *ptr"<<*ptr<<endl;
+    int n;
+    cout<<"enter size of arr : ";
+    cin>>n;
+    int arr[n][n];
+    cout<<"enter elements of array : ";
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cin>>arr[i][j];
+        }
+    }
+    cout<<endl<<"simple print "<<endl;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            cout<<arr[i][j]<<"\t";
+        }
+        cout<<"\n";
+    }
+    cout<<endl;
+    int k=0;
+    cout<<"required print : ";
+    for(int j=0;j<n;j++){
+        for(int i=0;i<n;i++){
+            if(j%2==0){
+                cout<<arr[i][j]<<"\t";
+                k+=1;
+            }
+            else{
+                k-=1;
+                cout<<arr[k][j]<<"\t";
+            }
+        }
+    }
 }
--- 2nd_YR/3rd_SEM/oops/zproject3sem/project.cpp
+++ 2nd_YR/3rd_SEM/oops/zproject3sem/project.cpp
@@ -9,14 +9,84 @@ using namespace std;
  
 //:::::::::::::::::::::::::::::::::::::to ask about how to go back to a function outside the class using the function inside the class
 
+class db{
+    public:
+        
+};
+
 class HOD{
     private:
+
+        void teacherdatabase(){
+            cout<<"teacher database";
+            //use the connected teacher database
+        }
+
+        void studentdatabase(){
+            cout<<"student database";
+            //use the connected student data base
+        }
+        
+        int power(string name){
+            system("cls");
+            cout.width(70);
+            cout<<"WELCOME HOD "<<name;
+            cout<<"what would like to do (enter choice number) : "<<endl;
+            cout<<"1. ACCESS STUDENT DATABASE"<<endl;
+            cout<<"2. ACCESS TEACHER DATABASE"<<endl;
+            cout<<"3. back to main"<<endl;
+            cout<<"4. exit"<<endl;
+            int choice;
+            cin>>choice;
+            while(choice!=1 && choice!=2 && choice!=3 && choice!=4){
+                cout<<"wrong choice !!!!";
+                sleep(1);
+                cout<<"\r                             \r";
+                cout<<"enter the coice again";
+                sleep(1);
+                cout<<"\r                           \r";
+                cin>>choice;
+            }
+            if(choice==1)   teachedatabase();
+            else if(choice==2)  studenntdatabase();
+            else if(choice==3) mainHOD();
+            else if(choice==4)  return 0;;
+            else    return 0;
+        }
+
 
     public:
         void mainHOD(){
             system("COLOR 0e");
             system("cls");
             cout<<"hello HOD";
+            string user,pass,name;
+            cout<<"PLS enter your name : ";
+            cin>>name;
+            cout<<"PLS VERIFY YOUR ID"<<endl;
+            cout<<"USERNAME : ";
+            cin>>user;
+            cout<<"PASSWORD : ";
+            cin>>pass;
+            string password=user+"@123";
+            cout<<"CHECKING ";
+            for(int i=0;i<5;i++){
+                cout<<(char)219;
+                sleep(1);
+            }
+            if(pass!=password){
+                mainHOD();
+            }
+            else{
+                int ch;
+                cout<<endl<<"1.Do you want to fetch records : ";
+                cout<<endl<<"2.Do you want to go back to main : ";
+                cout<<endl<<"3.Do you want to go exit : "<<endl;
+                cin>>ch;
+                if(ch==1) power(name);
+                // else if(ch==2)    mainmain();
+                else    return 0;
+            }
         }
 };
 
@@ -25,30 +95,34 @@ class teachers{
 
         void remove(){
             cout<<"remove";
+            //use the connected database 
             //to remove the required row from database;
         }
 
         void insert(){
             cout<<"insert";
+            //use the connected database
             //to insert required row into databbase;
         }
 
         void change(){
             cout<<"change";
+            //use the connected database
             //to change required row in database;
         }
 
-        void teach(string name){
-            system("COLOR 0e");
+        int teach(string name){
             system("cls");
             cout.width(70);
             cout<<"WELCOME TEACHER "<<name;
             cout<<"what would like to do (enter choice number) : "<<endl;
-            cout<<"1.insert new student information"<<endl;
-            cout<<"2.remove a student information"<<endl;
-            cout<<"3.change a student information"<<endl;
-            cout<<"4.back"<<endl;
+            cout<<"1. INSERT A NEW STUDENT INFORMATION"<<endl;
+            cout<<"2. REMOVE A STUDENT INFORMATION"<<endl;
+            cout<<"3. CHANGE A STUDENT INFORMATION"<<endl;
+            cout<<"4. BACK TO MAIN "<<endl;
+            cout<<"5. EXIT "<<endl;
             int choice;
+            cin>>choice;
             while(choice!=1 && choice!=2 && choice!=3 && choice!=4){
                 cout<<"wrong choice !!!!";
                 sleep(1);
@@ -62,6 +136,7 @@ class teachers{
             else if(choice==2)  remove();
             else if(choice==3)  change();
             else if(choice==4)  mainteach();
+            else    return 0;
         }
     public:
         int mainteach(){
@@ -87,31 +162,76 @@ class teachers{
             }
             else{
                 int ch;
-                cout<<endl<<"1.Do you want to go head : ";
-                cout<<endl<<"2.Do you want to go back : ";
-                cout<<endl<<"3.Do you want to go exit : ";
+                cout<<endl<<"1.Do you want to ACCESS STUDENT DATABASE : ";
+                cout<<endl<<"2.Do you want to GO BACK TO MAIN : ";
+                cout<<endl<<"3.Do you want to EXIT  : "<<endl;
                 cin>>ch;
                 if(ch==1) teach(name);
                 // else if(ch==2)    mainmain();
                 else    return 0;
             }
         }
+        
 };
 
 class students{
     private:
 
-    public:
-        void mainstudent(){
-            system("COLOR 0e");
+        void details(){
+            cout<<"details";
+            //use the connected database
+            //to fetch details from database
+        }
+
+        int stud(string name){
+            // system("COLOR 0f");
             system("cls");
-            cout<<"hello stud";
+            cout.width(70);
+            cout<<"WELCOME STUDENT "<<name<<endl;
+            details();
+        }
+
+    public:
+        int mainstudent(){
+            system("COLOR 0b");
+            system("cls");
+            cout<<"hello student"<<endl;
+            string user,pass,name;
+            cout<<"PLS enter your name : ";
+            cin>>name;
+            string clas;
+            cout<<"pls enter your class : ";
+            cin>>clas;
+            cout<<"PLS VERIFY YOUR ID"<<endl;
+            cout<<"USERNAME : ";
+            cin>>user;
+            cout<<"PASSWORD : ";
+            cin>>pass;
+            string password=user+"@123";
+            cout<<"CHECKING ";
+            for(int i=0;i<5;i++){
+                cout<<(char)219;
+                sleep(1);
+            }
+            if(pass!=password){
+                mainstudent();
+            }
+            else{
+                int ch;
+                cout<<endl<<"1.Do you want to ACCESS STUDENT DATABASE : ";
+                cout<<endl<<"2.Do you want to go BACK TO MAIN : ";
+                cout<<endl<<"3.Do you want to go EXIT : "<<endl;
+                cin>>ch;
+                if(ch==1) stud(name);
+                // else if(ch==2)    mainmain();
+                else    return 0;
+            }
         }
 };
 
 
 void mainmain(){
-    system("COLOR 0e");
+    system("COLOR 0f");
     system("cls");
     cout.width(55);
     cout<<right<<"HELLO"<<endl<<endl;;
