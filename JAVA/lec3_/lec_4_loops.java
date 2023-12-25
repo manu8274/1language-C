3e1d2f08c6287b25357bc8f9d06960b17d0c4939
manu8274 <manu8274@gmail.com>
2023-12-25 17:29:43 +0530

    commit

--- JAVA/lec3_/lec_4_loops.java
+++ JAVA/lec3_/lec_4_loops.java
@@ -1,8 +1,14 @@
 package JAVA.lec3_;
 
+import java.util.Scanner;
+
 public class lec_4_loops {
     public static void main(String args []){
-        Scanner s = new Scanner(System.in);
-        
+        Scanner s=new Scanner(System.in);
+        int n=10;
+        while(n>0){
+            System.out.println(n);
+            n=n-1;
+        }
     }
 }
