88d0b81606124ebbf703743f0392fe12149d28a1
manu kumar <manu1238274@gmail.com>
2023-12-30 19:41:35 +0530

    commit

--- JAVA/lec3_/lec_4_loops.java
+++ JAVA/lec3_/lec_4_loops.java
@@ -4,11 +4,36 @@ import java.util.Scanner;
 
 public class lec_4_loops {
     public static void main(String args []){
+        // Scanner s=new Scanner(System.in);
+        // int n=10;
+        // while(n>0){
+        //     System.out.println(n);
+        //     n=n-2;
+        // }
+//ASSIGNMENT QUETIONS
+    //Q1
+        // Scanner s=new Scanner(System.in);
+        // int ba;
+        // char grade;
+        // ba=s.nextInt();
+        // grade=s.next().charAt(0);
+        // double ta;
+        // int allow;
+        // if(grade=='A')  allow=1700;
+        // else if(grade=='B') allow=1500;
+        // else    allow=1300;
+        // ta=(ba) + (0.2*ba) + (0.5*ba) + allow - (0.11*ba);
+        // System.out.println((int)(Math.ceil(ta)));
+    //Q2
         Scanner s=new Scanner(System.in);
-        int n=10;
-        while(n>0){
-            System.out.println(n);
-            n=n-2;
+        int x;
+        x=s.nextInt();
+        int o=0,e=0;
+        while(x>0){
+            if((x%10)%2==0) e+=x%10;
+            else    o+=x%10;
+            x=x/10;
         }
+        System.out.println("even sum = " + e + " Odd sum = " + o);
     }
 }
