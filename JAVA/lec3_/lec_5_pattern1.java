89559cd803c94c46291e763435bec1d592b047d7
manu kumar <manu1238274@gmail.com>
2024-01-05 13:35:34 +0530

    commit

---
+++ JAVA/lec3_/lec_5_pattern1.java
@@ -0,0 +1,30 @@
+package JAVA.lec3_;
+
+import java.util.Scanner;
+
+public class lec_5_pattern1 {
+    public static void main(String args[]){
+//QUESTION DO 
+    //4.0 code square pattern 
+        // Scanner sc=new Scanner(System.in);
+        // int x;
+        // x=sc.nextInt();
+        // for(int i=0;i<x;i++){
+        //     for(int j=0;j<x;j++){
+        //         System.out.print(x + " ");
+        //     }
+        //     System.out.println();
+        // }
+    //6.0 code triangle pattern
+        // Scanner sc=new Scanner(System.in);
+        // int x;
+        // x=sc.nextInt();
+        // for(int i=1;i<=x;i++){
+        //     for(int j=0;j<i;j++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+    
+    }
+}
