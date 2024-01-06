9dba6fab6095101d0504d88918761571669e6e1f
manu kumar <manu1238274@gmail.com>
2024-01-06 13:53:49 +0530

    commit

---
+++ JAVA/lec3_/lec_6_pattern2.java
@@ -0,0 +1,29 @@
+package JAVA.lec3_;
+
+import java.util.Scanner;
+
+public class lec_6_pattern2 {
+    public static void main(String args []){
+//QUESTION DO
+    //2.0 mirror number pattern 
+        // Scanner sc = new Scanner(System.in);
+        // int x;
+        // x=sc.nextInt();
+        // for(int i=1;i<=x;i++){
+        //     for(int j=1;j<=x-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        // }
+    //4.0 inverted number triangle
+        // Scanner sc = new Scanner(System.in);
+        // int x;
+        // x=sc.nextInt();
+        // for(int i=x;i>0;i++){
+        //     for(int j=0;j<=)
+        // }
+    }
+}
