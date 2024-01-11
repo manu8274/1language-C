9dfb725f006d5383f73fd53d9dba0d193c1393bf
manu kumar <manu1238274@gmail.com>
2024-01-11 17:46:47 +0530

    commit

--- class/2nd_YR/4th_SEM/lec_2_DSA/class/01_JAN/10_jan_array.cpp
+++ class/2nd_YR/4th_SEM/lec_2_DSA/class/01_JAN/10_jan_array.cpp
@@ -2,26 +2,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main(){
-    int n;
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
-    int k;
-    cin>>k;
-    bool flag=false;
-    for(int i=0;i<n;i++){
-        if(arr[i]==k){
-            cout<<i<<" ";
-            flag=true;
-        }
-    }
-    if(flag==false){
-        cout<<"NOT FOUND";
-    }
-}
+// int main(){
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     int k;
+//     cin>>k;
+//     bool flag=false;
+//     for(int i=0;i<n;i++){
+//         if(arr[i]==k){
+//             cout<<i<<" ";
+//             flag=true;
+//         }
+//     }
+//     if(flag==false){
+//         cout<<"NOT FOUND";
+//     }
+// }
 
 
 ////2)BINARY SEARCH
@@ -57,22 +57,22 @@ int main(){
 
 //reversal of array
 
-// int reverse(int *arr,int size){
-//     for(int i=0;i<size/2;i++){
-//         swap(arr[i],arr[size-i-1]);
-//     }
-// }
+int reverse(int *arr,int size){
+    for(int i=0;i<size/2;i++){
+        swap(arr[i],arr[size-i-1]);
+    }
+}
 
-// int main(){
-//     int n;
-//     cin>>n;
-//     int arr[n];
-//     for(int i=0;i<n;i++){
-//         cin>>arr[i];
-//     }
-//     reverse(arr,n);
-//     cout<<"FINAL ARRAY:"<<endl;
-//     for(int i=0;i<n;i++){
-//         cout<<arr[i]<<" ";
-//     }
-// }
+int main(){
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    reverse(arr,n);
+    cout<<"FINAL ARRAY:"<<endl;
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
