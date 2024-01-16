2252410de1de4d601c258d9ba116208b0f6ff8c9
manu kumar <manu1238274@gmail.com>
2024-01-16 17:24:24 +0530

    commit

---
+++ competetive_progg/codeforces/courses/1_binary_search/simple_binary_search.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+// void bs(vector<int>arr,int c,int n){
+//     int s=0;
+//     int e=n-1;
+//     int mid;
+//     int fin=0;
+//     while(s<=e){
+//         mid=(s+e)/2;
+//         if(c==arr[mid]){
+//             fin=1;
+//             break;
+//         }
+//         else if(c>arr[mid]){
+//             s=mid+1;
+//         }
+//         else if(c<arr[mid]){
+//             e=mid-1;
+//         }
+//     }
+//     if(fin==1)  cout<<"YES\n";
+//     else    cout<<"NO\n";
+// }
+
+int main(){
+    int n;
+    int k;
+    cin>>n>>k;
+    vector<int>arr;
+    for(int i=0;i<n;i++){
+        int c;
+        cin>>c;
+        arr.push_back(c);
+    }
+    for(int i=0;i<k;i++){
+        int c;
+        cin>>c;
+        // bs(arr,c,arr.size());
+        int s=0;
+        int e=n-1;
+        int mid;
+        int fin=0;
+        while(s<=e){
+            mid=(s+e)/2;
+            if(c==arr[mid]){
+                fin=1;
+                break;
+            }
+            else if(c>arr[mid]){
+                s=mid+1;
+            }
+            else if(c<arr[mid]){
+                e=mid-1;
+            }
+        }
+        if(fin==1)  cout<<"YES\n";
+        else
+        {
+            cout<<"NO\n";
+        }
+    }
+}
