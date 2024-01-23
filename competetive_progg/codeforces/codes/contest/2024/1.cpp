a6de2bdb022754e563f9ca3397bdb6af26c055a9
manu kumar <manu1238274@gmail.com>
2024-01-23 17:32:33 +0530

    commit

--- competetive_progg/codeforces/codes/contest/2024/1.cpp
+++ competetive_progg/codeforces/codes/contest/2024/1.cpp
@@ -5,8 +5,50 @@ using namespace std;
 int main(){
     int t;
     cin>>t;
-    int arr[t];
-    for(int _=0;_<=t;_++){
-        
+    for(int _=0;_<t;_++){
+        int n;
+        cin>>n;
+        vector<int> lst;
+        for(int i=0;i<n;i++){
+            int o;
+            cin>>o;
+            lst.push_back(o);
+        }
+        int m;
+        cin>>m;
+        for(int i=0;i<m;i++){
+            int x,y;
+            cin>>x>>y;
+            int mc=0;
+            if(x<y){
+                x=x-1;
+                y=y-1;
+                for(int j=x;j<=y;j++){
+                    if(i==0)    mc++;
+                    else{
+                        if(abs(lst[i]-lst[i-1])>abs(lst[i+1]-lst[i])){
+                            mc+=1;
+                        }
+                        else{
+                            mc=mc+abs(lst[i+1]-lst[i]);
+                        }
+                    }
+                }
+            }
+            if(y<x){
+                x=x-1;
+                y=y-1;
+                for(int j=x;j=y;j--){
+                    if(j==n-1)  mc+=1;
+                    else{
+                        if(abs(lst[i-1]-lst[i])>abs(lst[i+1]-lst[i]))   mc++;
+                        else{
+                            mc+=abs(lst[i-1]-lst[i]);
+                        }
+                    }
+                }
+            }
+            cout<<mc<<endl;
+        }
     }
 }
---
+++ zzmisc/1.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include<math.h>
+
+using namespace std;
+
+int main(){
+    int n=5;
+    // cin>>n;
+    int arr[n]={1,4,2,5,3};
+    // for(int i=0;i<n;i++){
+    //     cin>>arr[i];
+    // }
+    vector<int>mlar;
+    for(int i=1;i<=n;i++){
+        if(i%2!=0){
+            if(2*i-1<=n)    mlar.push_back(i);
+            else    mlar.push_back(n-i+1);
+        }
+    }
+    int ts=0;
+    int x;
+    if(n%2==0)  x=n/2;
+    else    x=n/2+1;
+    
+    for(int i=0;i<x;i++){
+        int t=0;
+        if(n%2!=0 && i==n/2){
+            for(int j=0;j<mlar.size();j++){
+                t+=arr[i]*(fmin(i+1,mlar[j]));
+            }
+        }
+        else{
+            for(int j=0;j<mlar.size();j++){
+                t+=(arr[i]+arr[n-i-1])*(fmin(i+1,mlar[j]));
+            }
+        }
+        ts+=t;
+    }
+    cout<<ts;
+}
