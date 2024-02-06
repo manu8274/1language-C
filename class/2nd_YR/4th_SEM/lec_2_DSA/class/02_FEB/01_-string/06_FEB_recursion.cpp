837bc318183eb62c2acf67e177d1339871ebb984
manu kumar <manu1238274@gmail.com>
2024-02-06 18:07:55 +0530

    commit

--- class/2nd_YR/4th_SEM/lec_2_DSA/class/02_FEB/01_-string/06_FEB_recursion.cpp
+++ class/2nd_YR/4th_SEM/lec_2_DSA/class/02_FEB/01_-string/06_FEB_recursion.cpp
@@ -2,6 +2,14 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+//for using bigger problem we use recursion 
+
+int facto(int n){
+    if(n==1)    return 1;
+    else    return n*facto(n-1);
+}
+
 int main(){
-    
+    cout<<facto(5);
+    return 0;
 }
