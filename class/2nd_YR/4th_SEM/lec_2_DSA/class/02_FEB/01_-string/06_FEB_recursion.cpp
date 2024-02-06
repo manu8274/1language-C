535c516fbc6dbb0848c4433ab96ebe5b2fc736a0
manu kumar <manu1238274@gmail.com>
2024-02-06 18:27:09 +0530

    commit

--- class/2nd_YR/4th_SEM/lec_2_DSA/class/02_FEB/01_-string/06_FEB_recursion.cpp
+++ class/2nd_YR/4th_SEM/lec_2_DSA/class/02_FEB/01_-string/06_FEB_recursion.cpp
@@ -4,12 +4,24 @@ using namespace std;
 
 //for using bigger problem we use recursion 
 
-int facto(int n){
-    if(n==1)    return 1;
-    else    return n*facto(n-1);
+// int facto(int n){
+//     if(n==1)    return 1;
+//     else    return n*facto(n-1);
+// }
+
+// int main(){
+//     cout<<facto(5);
+//     return 0;
+// }
+
+bool check (int arr[],int n){
+    if(n==1 || n==0)  return true;
+    if(arr[0]<arr[1] && check(arr+1,n-1))   return true;
+    return false;
 }
 
 int main(){
-    cout<<facto(5);
-    return 0;
+    int arr[]={1,2,3,6,5};
+    bool b=check(arr,5);
+    cout<<b;
 }
