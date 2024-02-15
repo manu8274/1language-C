f35a4d4dee8f619ad0d894fbc9ca51cece0f1892
manu kumar <manu1238274@gmail.com>
2024-02-15 19:01:03 +0530

    commit

---
+++ class/2nd_YR/4th_SEM/lec_2_DSA/class/02_FEB/01_-string/15_stquestions.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+// int main(){
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+//     int x;
+//     cin>>x;
+//     sort(arr,arr+n);
+//     int count=0;
+//     for(int i=0;i<n-1;i++){
+//         if(arr[i+1]-arr[i]<=x){
+//             count++;
+//             i++;
+//         }
+//     }
+//     cout<<count;
+// }
+// int main(){
+//     int n;
+//     string s;
+//     cin>>s;
+//     n=s.size();
+//     bool f=true;
+//     for(int i=0;i<n/2;i++){
+//         if(s[i]!=s[n-i-1]){
+//             f=false;
+//             break;
+//         }
+//     }
+//     if(f==true) cout<<"TRUE";
+//     else    cout<<"FALSE";
+// }
+
