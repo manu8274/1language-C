8c2a23d3029b7cc311b580a5104f6fffcbce2f08
manu kumar <manu1238274@gmail.com>
2024-04-09 19:37:19 +0530

    commit

---
+++ class/2nd_YR/4th_SEM/lec_3_FEE_II/class/05_nodejs/04_april_1/myapp/src/App.js
@@ -0,0 +1,10 @@
+// import logo frośm './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <h2>HELLO WORLD</h2>
+  );
+}
+
+export default App;
--- class/2nd_YR/4th_SEM/lec_4_AIML/project/st2_project/main.py
+++ class/2nd_YR/4th_SEM/lec_4_AIML/project/st2_project/main.py
@@ -1,1 +1,4 @@
-print(2+2)
+import random,math,matplotlib.pyplot as plt,seaborn as sns,numpy as np,pandas as pd
+from sklearn.linear_model import LinearRegression as lr
+df=pd.read_csv("Housing.csv")
+print(df.head())
