f12d2bce93f93d0bf84933d2d132760fa2f39c83
manu kumar <manu1238274@gmail.com>
2024-04-10 18:21:31 +0530

    commit

---
+++ class/2nd_YR/4th_SEM/lec_2_DSA/class/09_tree/04_april/04_10.cpp
@@ -0,0 +1,71 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class node{
+    char data;
+    public:
+        node *left,*right;
+        // node():data(NULL),left(nullptr),right(nullptr)
+        // {}
+        node(){
+            right=left=nullptr;
+        }
+        node(char d): data(d), left(nullptr), right(nullptr) {}
+        char get_data(){
+            return data;
+        }
+};
+
+
+void inorder(node *r){
+    if(r==nullptr)  return;
+    inorder(r->left);
+    cout<<r->get_data()<<" ";
+    inorder(r->right);
+}
+
+void preorder(node *r){
+    if(r==nullptr)  return;
+    cout<<r->get_data()<<" ";
+    preorder(r->left);
+    preorder(r->right);
+}
+
+void postorder(node *r){
+    if(r==nullptr)  return;
+    postorder(r->left);
+    postorder(r->right);
+    cout<<r->get_data()<<" ";
+}
+
+void levelorder(node *r){
+    if(r==nullptr)  return;
+    cout<<r->get_data()<<" "<<r->left->get_data()<<" "<<r->right->get_data()<<" ";
+    levelorder(r->left);
+    levelorder(r->right);
+}
+
+// formula are left=2*i+1 , right=2*i+2
+
+node* arr_to_tree(int arr[]){
+    
+}
+
+int main(){
+    node *tree=new node('A');
+    tree->left=new node('B');
+    tree->left->left=new node('D');
+    tree->left->right=new node('E');
+    tree->right=new node('C');
+    tree->right->left=new node('F');
+    tree->right->right=new node('G');
+    cout<<endl<<"inorder"<<"\t";
+    inorder(tree);
+    cout<<endl<<"preorder"<<"\t";
+    preorder(tree);
+    cout<<endl<<"postorder"<<"\t";
+    postorder(tree);
+    cout<<endl<<"level order"<<"\t";
+    levelorder(tree);
+}
