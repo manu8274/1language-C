010982c5d1f06eeb3a4c01043e4f1cff9120e929
manu kumar <manu1238274@gmail.com>
2024-04-11 19:08:32 +0530

    commit

---
+++ class/2nd_YR/4th_SEM/lec_2_DSA/class/09_tree/04_april/04_11.cpp
@@ -0,0 +1,74 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+class node{
+    int data;
+    public:
+        node *left,*right;
+        // node():data(NULL),left(nullptr),right(nullptr)
+        // {}
+        node(){
+            // data=NULL;
+            right=left=nullptr;
+        }
+        node(int d): data(d), left(nullptr), right(nullptr) {}
+        int get_data(){
+            return data;
+        }
+};
+
+
+void inorder(node *r){
+    if(r==nullptr)  return;
+    inorder(r->left);
+    cout<<r->get_data()<<" ";
+    inorder(r->right);
+}
+
+void preorder(node *r){
+    if(r==nullptr)  return;
+    cout<<r->get_data()<<" ";
+    preorder(r->left);
+    preorder(r->right);
+}
+
+void postorder(node *r){
+    if(r==nullptr)  return;
+    postorder(r->left);
+    postorder(r->right);
+    cout<<r->get_data()<<" ";
+}
+
+
+// formula are left=2*i+1 , right=2*i+2
+
+node *build(int *arr, int i, int size,node *root){
+    if(i<size){
+        node *temp=new node(arr[i]);
+        root=temp;
+        root->left=build(arr,2*i+1,size,root->left);
+        root->right=build(arr,2*i+2,size,root->right);
+    }
+    return root;
+}
+
+int main(){
+    // node *tree=new node('A');
+    // tree->left=new node('B');
+    // tree->left->left=new node('D');
+    // tree->left->right=new node('E');
+    // tree->right=new node('C');
+    // tree->right->left=new node('F');
+    // tree->right->right=new node('G');
+    // cout<<endl<<"inorder"<<"\t";
+    // inorder(tree);
+    // cout<<endl<<"preorder"<<"\t";
+    // preorder(tree);
+    // cout<<endl<<"postorder"<<"\t";
+    // postorder(tree);
+    int arr[7]={1,2,3,4,5,6,7};
+    node * tree=nullptr;
+    tree=build(arr,0,7,tree);
+    preorder(tree);
+}
--- class/2nd_YR/4th_SEM/lec_3_FEE_II/class/05_nodejs/04_april_1/myapp/src/index.js
+++ class/2nd_YR/4th_SEM/lec_3_FEE_II/class/05_nodejs/04_april_1/myapp/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 import About from './Pages/About';
 import Contact from './Pages/Contact';
@@ -12,6 +12,7 @@ root.render(
   <BrowserRouter>
   <Routes>
     <Route path='/about' element={<About/>} />
+    <Route path='/contact' element={<Contact/>} />
   </Routes>
   </BrowserRouter>
 );
