36d1dc0066b6193fdda4e049aa73baf069817dc6
manu kumar <manu1238274@gmail.com>
2024-04-23 18:30:19 +0530

    commit

--- class/2nd_YR/4th_SEM/lec_2_DSA/class/10_heap/04_april/23_04.cpp
+++ class/2nd_YR/4th_SEM/lec_2_DSA/class/10_heap/04_april/23_04.cpp
@@ -121,9 +121,58 @@ class max_heap{
             }
             cout<<endl;
         }
+        void heapify1(int i){
+            if(2*i > v.size()){
+                return ;
+            }
+            else if(2*i+1>v.size()){
+                if(v[i]<v[2*i]){
+                    swap(v[i],v[2*i]);
+                    heapify1(2*i);
+                }
+            }
+            else{
+                int ind;
+                if(v[2*i]<v[2*i+1]){
+                    ind=2*i+1;
+                }
+                else{
+                    ind=2*i;
+                }
+                swap(v[i],v[ind]);
+                heapify1(ind);
+            }
+            return ;
+        }
+        void heapify(int i){
+            int left=2*i;
+            int right=2*i+1;
+            int min=i;
+            if(left<v.size() && v[left]>v[min]){
+                min=left;
+            }
+            if(right<v.size() && v[right]>v[min]){
+                min=right;
+            }
+            if(i!=min){
+                swap(v[min],v[i]);
+                heapify(min);
+            }
+        }
+
+        void pop(){
+            // step1 swap 1st and last element;
+            // step2 popback
+            // step3 heapify;
+            // heapify:we check leftv and right index and swap with min untill recursive end;
+            swap(v[1],v[v.size()-1]);
+            v.pop_back();
+            heapify(1);
+        }
 };
 
 int main(){
+    cout<<"MIN HEAP"<<endl;
     min_heap h1;
     h1.push(10);
     h1.push(20);
@@ -134,4 +183,15 @@ int main(){
     h1.print();
     h1.pop();
     h1.print();
+    cout<<"MAX HEAP"<<endl;
+    max_heap h2;
+    h2.push(10);
+    h2.push(20);
+    h2.push(5);
+    h2.push(2);
+    h2.push(6);
+    h2.push(16);
+    h2.print();
+    h2.pop();
+    h2.print();
 }
