7429a9c62703ec0dc19d5c09e92853e70e24ad8f
manu kumar <manu1238274@gmail.com>
2024-08-04 01:00:08 +0530

    commit

--- UCA/SEM5/JAVA/RITESH_SIR/08_03.cpp
+++ UCA/SEM5/JAVA/RITESH_SIR/08_03.cpp
@@ -1,9 +1,12 @@
 #include <stdio.h>
+#include<math.h>
 
 int main()
 {
-    int n = -10;
-    char c = (char)n;
-    n = n >> 31;
-    printf("%d", n);
+    // int n = 10;
+    // char c = (char)n;
+    // printf("%d", n >> 31 | n);
+    //
+    int a = -128;
+    printf("%d", a | );
 }
